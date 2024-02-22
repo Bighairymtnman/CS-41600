735fb5a9090624860fe13ae5407c28be295455e7
Bighairymtnman <158404232+Bighairymtnman@users.noreply.github.com>
2024-02-22 15:45:17 -0600

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,8 +1,8 @@
 public class CS416 {
 
    
-    public static void Info() {
-       
+    public static void Info() 
+   {
         String yourName = "Your Name";
         System.out.println("Name: " + "Andrew Nicastro");
     }
