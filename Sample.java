ea864f3edf1986e61c717512c3d3ba480844eca4
Bighairymtnman <158404232+Bighairymtnman@users.noreply.github.com>
2024-02-22 15:42:29 -0600

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,1 +1,16 @@
-system.out.println("SAMPLE");
+public class CS416 {
+
+   
+    public static void Info() {
+       
+        String yourName = "Your Name";
+        System.out.println("Name: " + "Andrew Nicastro");
+    }
+
+  
+    public static void main(String[] args) {
+        // Call your method
+        Info();
+    }
+}
+
